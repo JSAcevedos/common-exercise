1512d80adffc060807ffcfb4b2a2725c65edd683
Juan Acevedo <juan.acevedo9805@gmail.com>
2023-03-17 09:15:01 -0500

    Linkedlist and nodes implementation

---
+++ main.py
@@ -0,0 +1,47 @@
+#Nodes implementation
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+#Linked list implementation
+class linkedList:
+    
+    def __init__(self):
+      self.head = None
+      self.tail = None
+    
+    def pushback(self, data):
+        newNode = Node(data)
+        if self.head == None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            self.tail.next = newNode
+            self.tail = newNode
+
+    def popback(self):
+        if self.tail is None:
+            print("ERROR: Empty list")
+        else:
+            current_node = self.head
+            while current_node.next.next:
+                current_node = current_node.next
+            temp = current_node.data
+            current_node.next = None
+            self.tail = current_node
+            return temp
+
+
+    def print(self):
+        if self.head is None:
+            print("ERROR: Empty list")
+        else:
+            current_node = self.head
+            while current_node is not None:
+                if current_node.next is None:
+                    print(current_node.data, end="")
+                else:
+                    print(current_node.data, end=" ")
+                current_node = current_node.next
+
